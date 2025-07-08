c9b0d3f25d2fd4f29d7243f71faf6aea4c954c9f
Webbney Vallon <webbvallon1999@gmail.com>
2025-07-08 00:35:33 -0400

    created sign in component

---
+++ src/SignUpForm.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router";
+
+
+
+const SignUp = () => {
+    return (
+
+    <div>
+        <h1>Sign up</h1>
+    </div>
+
+
+    );
+};
+
+export SignUp;
